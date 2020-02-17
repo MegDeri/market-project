b63c254bb6c3c161ea5154acba6ffa52d38a5bb8
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2020-02-17 21:24:38 +0200

    Modify server

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ const config = require("./config");
 const mongoose = require("mongoose");
 const loadTestData = require("./testData");
 const helmet = require("helmet");
-//const sanitize = require('mongo-sanitize');
+const sanitize = require("mongo-sanitize");
+const path = require("path");
 
 const app = express();
 
@@ -15,6 +16,19 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/api", productRoutes);
+
+app.use((req, res, next) => {
+  mongoSanitize(req.body);
+  next();
+});
+
+//serve static files from react app
+app.use(express.static(path.join(__dirname, "/../client/build")));
+
+//use path from react app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../client/build/index.html"));
+});
 
 // connects back-end code with database
 mongoose.connect(config.DB, { useNewUrlParser: true });
