846e27e1a8fe126418696823c9aae01f35bcc924
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2020-02-18 14:13:06 +0200

    Modify config

---
+++ client/build/precache-manifest.f6e21c9d3e9c5bc71c8241d41d99aadd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "447da63880132ef03dd6b87bbc28cb6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1974817d04c52d47e951",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "4355f3512a4c3fd4c2c3",
+    "url": "/static/css/main.1b5f80b4.chunk.css"
+  },
+  {
+    "revision": "1974817d04c52d47e951",
+    "url": "/static/js/2.e5b59278.chunk.js"
+  },
+  {
+    "revision": "4355f3512a4c3fd4c2c3",
+    "url": "/static/js/main.72d9c9bb.chunk.js"
+  },
+  {
+    "revision": "cbee8d858c084296ccff",
+    "url": "/static/js/runtime-main.985e6003.js"
+  }
+]);
--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
-// export const API_URL = "/api";
-// export const BASE_URL = "https://market-project.herokuapp.com";
-export const API_URL = "http://localhost:8000/api";
+export const API_URL = "/api";
+export const BASE_URL = "https://market-project.herokuapp.com";
+//export const API_URL = "http://localhost:8000/api";
