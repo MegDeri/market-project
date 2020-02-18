c74b4f01777189694ba85b497942434769d04e8a
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2020-02-18 12:45:38 +0200

    Modify App

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,33 @@
-import React from 'react';
+import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-import  { MainLayout } from './components/layout/MainLayout/MainLayout';
+import { MainLayout } from "./components/layout/MainLayout/MainLayout";
 
 //import routes
-import { Home } from './components/pages/Home/Home';
-import  Products from './components/pages/Product/ProductsContainer';
-import  SingleProductPage  from './components/pages/SingleProductPage/SingleProductPage';
-import { ContactPage } from './components/pages/Contact/ContactPage';
-import Faq from './components/pages/FAQ/FAQ';
-import CartPage from './components/pages/CartPage/CartPage';
-import { NotFoundPage } from './components/pages/NotFound/NotFoundPage';
-import Logo from './components/common/Logo/Logo';
+import { Home } from "./components/pages/Home/Home";
+import Products from "./components/pages/Product/ProductsContainer";
+import SingleProductPage from "./components/pages/SingleProductPage/SingleProductPage";
+import { ContactPage } from "./components/pages/Contact/ContactPage";
+import Faq from "./components/pages/FAQ/FAQ";
+import CartPage from "./components/pages/CartPage/CartPage";
+import { NotFoundPage } from "./components/pages/NotFound/NotFoundPage";
+import Logo from "./components/common/Logo/Logo";
 
 class App extends React.Component {
-  
   render() {
     return (
-      
       <MainLayout>
         <Switch>
           <Route exact path={"/"} component={Home} />
           <Route exact path={"/"} component={Logo} />
-          <Route exact path={"/product"} component={Products}/>
-          <Route exact path={"/product/:id"} component={SingleProductPage} />
+          <Route exact path={"/products"} component={Products} />
+          <Route exact path={"/products/:id"} component={SingleProductPage} />
           <Route exact path={"/faq"} component={Faq} />
           <Route exact path={"/contact"} component={ContactPage} />
           <Route exact path={"/cart"} component={CartPage} />
           <Route exact component={NotFoundPage} />
         </Switch>
-     </MainLayout>
-     
+      </MainLayout>
     );
   }
 }
