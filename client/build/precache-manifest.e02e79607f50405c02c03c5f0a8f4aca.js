06415d59aeae330925d6f586c711b900aebb6b93
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2020-02-19 16:12:43 +0200

    Add build

---
+++ client/build/precache-manifest.e02e79607f50405c02c03c5f0a8f4aca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1bcc0bf09e279b2983406622d7b0d3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0271f837f93ec19da9ef",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "af200824c803d9f2774a",
+    "url": "/static/css/main.fa9726c2.chunk.css"
+  },
+  {
+    "revision": "0271f837f93ec19da9ef",
+    "url": "/static/js/2.3e880f8f.chunk.js"
+  },
+  {
+    "revision": "af200824c803d9f2774a",
+    "url": "/static/js/main.01b8fb20.chunk.js"
+  },
+  {
+    "revision": "cbee8d858c084296ccff",
+    "url": "/static/js/runtime-main.985e6003.js"
+  }
+]);
